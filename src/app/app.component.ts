7ce6299ccfc14960b76dc2db361d16a15ca53954
Szeto <swl58109@gmail.com>
2018-08-09 11:40:33 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor() {  }
+}
